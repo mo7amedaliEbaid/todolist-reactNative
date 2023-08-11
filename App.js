300529dda9cbd987a275d04a05bc09d158975ed9
devAhmed94 <ahmedali26002844@gmail.com>
2023-08-11 04:59:59 +0300

    finish

--- App.js
+++ App.js
@@ -1,44 +1,106 @@
 import { StatusBar } from 'expo-status-bar';
-import React  from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Task  from './components/Task';
+import React, { useState } from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-    <View style={styles.tasksWrapper}>
-<Text style={styles.sectionTitle}>
-    Todays tasks
-</Text>
-<View style={styles.items}>
-    <Task text={'Task 1'} />
-    <Task text={'Task 2'} />
-    <Task text={'Task 3'} />
-</View>
-    </View>
-    </View>
-  );
+    const [task, setTask] = useState();
+    const [taskItems, setTaskItems] = useState([]);
+    const handleAddTask = () => {
+        Keyboard.dismiss();
+        setTaskItems([...taskItems, task])
+        setTask(null);
+    }
+
+    const completeTask = (index) => {
+        let itemsCopy = [...taskItems];
+        itemsCopy.splice(index, 1);
+        setTaskItems(itemsCopy);
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.tasksWrapper}>
+                <Text style={styles.sectionTitle}>
+                    Today's tasks
+                </Text>
+                <View style={styles.items}>
+                    {
+                        taskItems.map((item, index) => {
+                            return (
+                                <TouchableOpacity key={index} onPress={() => completeTask(index)}> <Text text={item} />  </TouchableOpacity>
+                            )
+                        })
+                    }
+
+                </View>
+            </View>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.writeTaskWrapper}>
+                <TextInput style={styles.input} placeholder={'Write a task'}
+                    value={task}
+                    onChangeText={text => setTask(text)}
+                />
+                <TouchableOpacity onPress={() => handleAddTask()}>
+                    <View style={styles.addWrapper}>
+                        <Text style={styles.addText}> + </Text>
+                    </View>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:'#c8e8e5',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between',
-  },
-  tasksWrapper:{
-    paddingTop:80,
-    paddingHorizontal:20,
-    width:320,
-    alignContent:'center'
-  },
-  sectionTitle:{
-    fontSize:24,
-    fontWeight:'bold'
-  },
-  items:{
-    marginTop:30,
+    container: {
+        flex: 1,
+        backgroundColor: '#c8e8e5',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+    },
+    tasksWrapper: {
+        paddingTop: 80,
+        paddingHorizontal: 20,
+        width: 320,
+        alignContent: 'center'
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    items: {
+        marginTop: 30,
 
-  },
+    },
+    writeTaskWrapper: {
+        position: 'absolute',
+        bottom: 60,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    input: {
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        width: 250,
+        backgroundColor: '#f0e878',
+        borderRadius: 60,
+        borderColor: '#17160d',
+        borderWidth: 1
+    },
+    addWrapper: {
+        width: 60,
+        height: 60,
+        backgroundColor: '#f0e878',
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#17160d',
+        borderWidth: 1
+    },
+    addText: {
+
+    },
+
 }); 
